2d4d7bfd8f5484c7fcb65caa6d93f2d6696fd490
lordevito <lollo.devito@hotmail.com>
2023-12-12 00:47:08 +0100

    feat: font (da fixare)

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './styles/Style.scss'
 import App from './App.vue'
-
+import './styles/Style.scss'
+import './styles/Fonts.scss'
 createApp(App).mount('#app')
